636ce37af541e63b78b1153f3d4880e38d96b256
koazy0 <koazy_0@163.com>
2022-10-27 09:21:42 +0800

    IAT_HOOK

--- sourcecode/1.3/hook_iat/dllmain.cpp
+++ sourcecode/1.3/hook_iat/dllmain.cpp
@@ -7,8 +7,38 @@ LPCWSTR TargetDll = L"USER32.dll";
 FARPROC TargetFun;
 CString strTmp;
 
-BOOL HookIat(FARPROC Target, FARPROC HookFun){
+BOOL HookIat(LPCTSTR szDllname,FARPROC Target, FARPROC HookFun){
+	HMODULE hMod;
+	LPCTSTR szLibName;
+	PIMAGE_IMPORT_DESCRIPTOR pImage;
+	PIMAGE_THUNK_DATA pThunk;
+	DWORD dwOldProtect, dwRVA;
+	PBYTE pAddr;
 
+	hMod = GetModuleHandle(szDllname);
+	pAddr = (PBYTE) hMod;
+
+	pAddr += *((DWORD*)&pAddr[0x3C]);
+	dwRVA = *((DWORD*)&pAddr[0x80]);
+
+	pImage = (PIMAGE_IMPORT_DESCRIPTOR)((DWORD)hMod + dwRVA);
+
+	for (; pImage->Name; pImage++) {
+		szLibName = (LPCTSTR)((DWORD)hMod + pImage->Name);
+		if (!StrCmp(szLibName, szDllname)) {
+			pThunk = (PIMAGE_THUNK_DATA)((DWORD)hMod + pImage->FirstThunk);
+			for (; pThunk->u1.Function; pThunk++) {
+				if (pThunk->u1.Function == (DWORD)Target) {
+					VirtualProtect((LPVOID)pThunk->u1.Function, 4, PAGE_EXECUTE_READWRITE, &dwOldProtect);
+					pThunk->u1.Function = (DWORD)HookFun;
+					VirtualProtect((LPVOID)pThunk->u1.Function, 4, dwOldProtect, &dwOldProtect);
+					return TRUE;
+				}
+			}
+		}
+	}
+	
+	return FALSE;
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
@@ -16,10 +46,11 @@ BOOL APIENTRY DllMain( HMODULE hModule,
                        LPVOID lpReserved
                      )
 {
+	HMODULE hDll = GetModuleHandle(TargetDll);
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-		HMODULE hDll = GetModuleHandle(TargetDll);
+		
 		if (!hDll) {
 			strTmp.Format(L"GetModuleHandle Error:%c ",GetLastError());
 			MessageBox(NULL, strTmp, L"Error", MB_OK);
@@ -30,7 +61,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 			MessageBox(NULL, strTmp, L"Error", MB_OK);
 		}
 
-		HookIat();
+		HookIat(TargetDll,TargetFun, TargetFun);
 		break;
 
     case DLL_THREAD_ATTACH:
